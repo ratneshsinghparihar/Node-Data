76d6a6938d94fe62b614bfba31ad15a26e3099e1
ritesh <ritesh.agarwal@talentica.com>
2016-02-25 18:38:35 +0530

    token based implementation, with refresh token

--- dynamic/dynamic-controller.ts
+++ dynamic/dynamic-controller.ts
@@ -4,15 +4,33 @@ var express = require('express');
 import {DynamicRepository} from './dynamic-repository';
 var Reflect = require('reflect-metadata');
 export var router = express.Router();
-import {ISearchPropertyMap,GetAllFindBySearchFromPrototype} from "../decorators/metadata/searchUtils";
+var jwt = require('jsonwebtoken');
+import {ISearchPropertyMap, GetAllFindBySearchFromPrototype} from "../decorators/metadata/searchUtils";
 import * as Utils from "../decorators/metadata/utils";
 var Enumerable: linqjs.EnumerableStatic = require('linq');
 import {SecurityConfig} from '../security-config';
 
-var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+var loggedIn = require('connect-ensure-login').ensureLoggedIn;
+var expressJwt = require('express-jwt');
+var authenticateByToken = expressJwt({
+    secret: SecurityConfig.tokenSecretkey
+});
+
+var ensureLoggedIn = () => {
+
+    //by token
+    if (Config.Security.isAutheticationByToken) {
+        return authenticateByToken;
+    }
+
+    //by password
+    if (Config.Security.isAutheticationByUserPasswd) {
+        return loggedIn();
+    }
+}
 
 if (!Config.Security.isAutheticationEnabled) {
-     ensureLoggedIn = function () {
+    ensureLoggedIn = () => {
         return function (req, res, next) {
             next();
         }
@@ -30,65 +48,64 @@ export class DynamicController {
         this.addRoutes();
     }
 
-    
+
 
     addRoutes() {
         router.get(this.path,
             ensureLoggedIn(),
             (req, res) => {
-            
-            if(!this.isAuthorize(req,1))                    
-            return res.send(401, 'unauthorize access for resource ' + this.path);
+                if(!this.isAuthorize(req,1))                    
+                return res.send(401, 'unauthorize access for resource ' + this.path);
                 return this.repository.findAll()
                     .then((result) => {
                         result = this.getHalModels(result, this.repository.modelName());
                         this.sendresult(req, res, result);
 
                     });
-            });                 
-        
+            });
+
         router.get(this.path + '/:id',
-        ensureLoggedIn(),
-         (req, res) => {                          
-            if(!this.isAuthorize(req,1))                    
-            return res.send(401, 'unauthorize access for resource ' + this.path);      
-            return this.repository.findOne(req.params.id)
-                .then((result) => {
-                    this.getHalModel1(result,this.repository.modelName(),this.repository.getEntityType());
-                    this.sendresult(req, res, result);
-                });
-        });
+            ensureLoggedIn(),
+            (req, res) => {
+                if (!this.isAuthorize(req, 1))
+                    return res.send(401, 'unauthorize access for resource ' + this.path);
+                return this.repository.findOne(req.params.id)
+                    .then((result) => {
+                        this.getHalModel1(result, this.repository.modelName(), this.repository.getEntityType());
+                        this.sendresult(req, res, result);
+                    });
+            });
 
         router.get(this.path + '/:id/:prop',
-        ensureLoggedIn(),
-         (req, res) => {
-            return this.repository.findChild(req.params.id, req.params.prop)
-                .then((result) => {
-                    //result=this.getHalModel1(result,this.repository.modelName(),this.repository.getEntityType());
-                    //var propTypeName = Reflect.getMetadata("design:type", result.toObject()[req.params.prop], req.params.prop);
-                    this.getHalModel1(result,this.repository.modelName(),this.repository.getEntityType());
-                    
-                    var parent=(<any>result).toObject();
-                    var association=parent[req.params.prop];
-                    //var propName=Reflect.getMetadata("design:type", association, req.params.prop);
-                   // var resourceName= Reflect.getMetadata("design:type", association);
-                    //this.getHalModel(association,req.params.prop);
-                    this.sendresult(req, res,association );
-                });
-        });
+            ensureLoggedIn(),
+            (req, res) => {
+                return this.repository.findChild(req.params.id, req.params.prop)
+                    .then((result) => {
+                        //result=this.getHalModel1(result,this.repository.modelName(),this.repository.getEntityType());
+                        //var propTypeName = Reflect.getMetadata("design:type", result.toObject()[req.params.prop], req.params.prop);
+                        this.getHalModel1(result, this.repository.modelName(), this.repository.getEntityType());
+
+                        var parent = (<any>result).toObject();
+                        var association = parent[req.params.prop];
+                        //var propName=Reflect.getMetadata("design:type", association, req.params.prop);
+                        // var resourceName= Reflect.getMetadata("design:type", association);
+                        //this.getHalModel(association,req.params.prop);
+                        this.sendresult(req, res, association);
+                    });
+            });
 
         router.post(this.path,
-        ensureLoggedIn(),
-         (req, res) => {
-            this.getModelFromHalModel(req.body);
-            return this.repository.post(req.body)
-                .then((result) => {
-                    this.sendresult(req, res, result);
-                }).catch(error => {
-                    console.log(error);
-                    res.send(error);
-                });;
-        });
+            ensureLoggedIn(),
+            (req, res) => {
+                this.getModelFromHalModel(req.body);
+                return this.repository.post(req.body)
+                    .then((result) => {
+                        this.sendresult(req, res, result);
+                    }).catch(error => {
+                        console.log(error);
+                        res.send(error);
+                    });;
+            });
         
         
 
@@ -110,152 +127,148 @@ export class DynamicController {
 
         // add or update any property value
         router.put(this.path + "/:id",
-        ensureLoggedIn(),
-         (req, res) => {
-            return this.repository.put(req.params.id, req.body)
-                .then((result) => {
-                    this.sendresult(req, res, result);
-                }, (e) => {
-                    console.log(e);
-                });
-        });
+            ensureLoggedIn(),
+            (req, res) => {
+                return this.repository.put(req.params.id, req.body)
+                    .then((result) => {
+                        this.sendresult(req, res, result);
+                    }, (e) => {
+                        console.log(e);
+                    });
+            });
 
         router.delete(this.path + "/:id",
-        ensureLoggedIn(),
-         (req, res) => {
-            return this.repository.delete(req.params.id)
-                .then((result) => {
-                    this.sendresult(req, res, result);
-                }).catch(error => {
-                    res.send(error)
-                });;
-        });
+            ensureLoggedIn(),
+            (req, res) => {
+                return this.repository.delete(req.params.id)
+                    .then((result) => {
+                        this.sendresult(req, res, result);
+                    }).catch(error => {
+                        res.send(error)
+                    });;
+            });
 
         router.patch(this.path + "/:id",
-        ensureLoggedIn(),
-         (req, res) => {
-            return this.repository.patch(req.params.id, req.body)
+            ensureLoggedIn(),
+            (req, res) => {
+                return this.repository.patch(req.params.id, req.body)
+                    .then((result) => {
+                        this.sendresult(req, res, result);
+                    });
+            });
+
+    }
+
+    addSearchPaths() {
+        var modelRepo = this.repository.getModelRepo();
+        var searchPropMap = GetAllFindBySearchFromPrototype(modelRepo);
+        var links = { "self": { "href": "/search" } };
+        searchPropMap.forEach(map=> {
+            this.addRoutesForAllSearch(map);
+            links[map.key] = { "href": "/" + map.key, "params": map.args };
+        });
+        router.get(this.path + "/search", function (req, res) {
+            res.set("Content-Type", "application/json");
+            res.send(JSON.stringify(links, null, 4));
+        });
+    }
+
+    private addRoutesForAllSearch(map: ISearchPropertyMap) {
+        router.get(this.path + "/search/" + map.key, (req, res) => {
+            var query = req.query;
+            return this.repository
+                .findWhere(query)
                 .then((result) => {
                     this.sendresult(req, res, result);
                 });
-        });
 
-    }
-
-    addSearchPaths(){
-        var modelRepo = this.repository.getModelRepo();
-        var searchPropMap = GetAllFindBySearchFromPrototype(modelRepo);
-        var links = {"self": { "href": "/search"} };
-        searchPropMap.forEach(map=>{
-            this.addRoutesForAllSearch(map);
-            links[map.key] = {"href": "/"+map.key , "params" : map.args};
-        });
-        router.get(this.path+"/search",function(req,res){
-            res.set("Content-Type", "application/json");
-            res.send(JSON.stringify(links,null,4));
         });
     }
 
-    private addRoutesForAllSearch(map : ISearchPropertyMap){
-        router.get(this.path + "/search/" + map.key, (req,res)=>{
-            var query = req.query;
-            return this.repository
-            .findWhere(query)
-            .then((result)=>{
-                this.sendresult(req,res,result);
-            });
-            
-        });
-    }
-    
-    
-    private getHalModel(model:any,resourceName:string):any{
-        var selfUrl={};
-        selfUrl["href"]="/"+resourceName+"/"+model._id;
-        var selfObjec={};
-         selfObjec["self"]=selfUrl;      
-        model["_links"]=selfObjec;
-        
+
+    private getHalModel(model: any, resourceName: string): any {
+        var selfUrl = {};
+        selfUrl["href"] = "/" + resourceName + "/" + model._id;
+        var selfObjec = {};
+        selfObjec["self"] = selfUrl;
+        model["_links"] = selfObjec;
+
         return model;
     }
-    
-    private getModelFromHalModel(model:any)
-    {
-        if(model["_lniks"])
-        {
+
+    private getModelFromHalModel(model: any) {
+        if (model["_lniks"]) {
             delete model["_lniks"];
         }
     }
-    
-     
-    private getHalModel1(model:any,resourceName:string,resourceType:any):any{
-        var dbModel=model;
-        var entityModel:any =new resourceType(dbModel);
-        var selfUrl={};
-        selfUrl["href"]="/"+resourceName+"/"+model._id;
+
+
+    private getHalModel1(model: any, resourceName: string, resourceType: any): any {
+        var dbModel = model;
+        var entityModel: any = new resourceType(dbModel);
+        var selfUrl = {};
+        selfUrl["href"] = "/" + resourceName + "/" + model._id;
         //var selfObjec={};
         // selfObjec["self"]=selfUrl;      
-        entityModel["_links"]["self"]=selfUrl;
-        model=entityModel;
+        entityModel["_links"]["self"] = selfUrl;
+        model = entityModel;
         return model;
-    }    
-    
-    
-    private getHalModels(models: Array<any>,resourceName:string): any{
-        var halresult={};
-        halresult["_links"]={"self": { "href": "/"+resourceName},"search":{"href":"/search"}};
+    }
+
+
+    private getHalModels(models: Array<any>, resourceName: string): any {
+        var halresult = {};
+        halresult["_links"] = { "self": { "href": "/" + resourceName }, "search": { "href": "/search" } };
         models.forEach(model => {
-            this.getHalModel(model,resourceName);
+            this.getHalModel(model, resourceName);
         });
-        halresult["_embedded"]=models;
+        halresult["_embedded"] = models;
         return halresult;
     }
 
     private sendresult(req, res, result) {
         res.set("Content-Type", "application/json");
-        
-        res.send(JSON.stringify(result,null,4));
+
+        res.send(JSON.stringify(result, null, 4));
     }
-    
-    private isAuthorize(req:any, access: number):boolean
-    {
-        if(!Config.Security.isAutheticationEnabled)
-        return true; 
-        var isAutherize:boolean=false;
+
+    private isAuthorize(req: any, access: number): boolean {
+        if (!Config.Security.isAuthorisationEnabled)
+            return true;
+        var isAutherize: boolean = false;
         //check for autherization
-             //1. get resource name         
-             var resourceName=Utils.getResourceNameFromModel(this.repository.getEntityType())
-             //2. get auth config from security config
-             var authCofig=Enumerable.from(SecurityConfig.ResourceAccess)
-                                     .where((resourceAccess:any) => {return resourceAccess.name==resourceName ;}  )
-                                     .firstOrDefault();
-             //if none found then carry on                                     
-             if(authCofig)
-             {
+        //1. get resource name         
+        var resourceName = Utils.getResourceNameFromModel(this.repository.getEntityType())
+        //2. get auth config from security config
+        var authCofig = Enumerable.from(SecurityConfig.ResourceAccess)
+            .where((resourceAccess: any) => { return resourceAccess.name == resourceName; })
+            .firstOrDefault();
+        //if none found then carry on                                     
+        if (authCofig) {
                  
-                  //3. get user object in session
-                 var userInsession=req.user;
-                  //4. get roles for current user
+            //3. get user object in session
+            var userInsession = req.user;
+            //4. get roles for current user
                   
-                  if(!userInsession._doc.rolenames) return false;
-                  
-                  var userRoles:string=userInsession._doc.rolenames;
-                 
-                 var rolesForRead:Array<any>=Enumerable.from(authCofig.acl)
-                                     .where((acl:any) => {return (acl.accessmask & 1)==1  ;})
-                                     .toArray();
-                 //5 match auth config and user roles 
-                 rolesForRead.forEach(element => {
-                     if( userRoles.indexOf(element.role) >= 0){
-                                isAutherize=true;
-                            }                   
-                    
-                       
-                 });
-                 return   isAutherize;
-             }  
-                 
-                 return true;                    
+            if (!userInsession._doc.rolenames) return false;
+
+            var userRoles: string = userInsession._doc.rolenames;
+
+            var rolesForRead: Array<any> = Enumerable.from(authCofig.acl)
+                .where((acl: any) => { return (acl.accessmask & 1) == 1; })
+                .toArray();
+            //5 match auth config and user roles 
+            rolesForRead.forEach(element => {
+                if (userRoles.indexOf(element.role) >= 0) {
+                    isAutherize = true;
+                }
+
+
+            });
+            return isAutherize;
+        }
+
+        return true;
     }
-    
+
 }
--- security-config.ts
+++ security-config.ts
@@ -1,34 +1,37 @@
 
-export class SecurityConfig{
-  public static ResourceAccess : any=[{
-                                        "name":"blogs",
-                                        "acl" :[{"role":"ROLE_ADMIN","accessmask":7},
-                                                 {"role":"ROLE_USER","accessmask":1},
-                                                 {"role":"ROLE_AUTHOR","accessmask":7}
-                                                ]                                          
-                                        },
-                                         {"name":"comment",
-                                         "acl" :[{"role":"ROLE_ADMIN","accessmask":7},
-                                                 {"role":"ROLE_USER","accessmask":7},
-                                                 {"role":"ROLE_AUTHOR","accessmask":7}
-                                                ]  
-                                         }];
-public static secretkey : string= 'ericthered';
-public static issuer:string = "accounts.examplesoft.com";
-public static audience:string = "yoursite.net";                                        
+export class SecurityConfig {
+    public static ResourceAccess: any = [{
+        "name": "blogs",
+        "acl": [{ "role": "ROLE_ADMIN", "accessmask": 7 },
+            { "role": "ROLE_USER", "accessmask": 1 },
+            { "role": "ROLE_AUTHOR", "accessmask": 7 }
+        ]
+    },
+        {
+            "name": "comment",
+            "acl": [{ "role": "ROLE_ADMIN", "accessmask": 7 },
+                { "role": "ROLE_USER", "accessmask": 7 },
+                { "role": "ROLE_AUTHOR", "accessmask": 7 }
+            ]
+        }];
+    public static tokenSecretkey: string = 'ericthered';
+    public static tokenExpiresInMinutes: number = 2;//2 months
+
+    public static issuer: string = "accounts.examplesoft.com";
+    public static audience: string = "yoursite.net";
 }
 
 
- export enum AccessMask {
-        view = 1,
-        edit = 2,
-        delete = 4,
-        approve = 8
-    };
-    
-    export enum RoleEnum {
-        ROLE_ADMIN = 1,
-        ROLE_USER,
-        ROLE_AUTHOR,
-        ROLE_PUBLISHER
-    }
+export enum AccessMask {
+    view = 1,
+    edit = 2,
+    delete = 4,
+    approve = 8
+};
+
+export enum RoleEnum {
+    ROLE_ADMIN = 1,
+    ROLE_USER,
+    ROLE_AUTHOR,
+    ROLE_PUBLISHER
+}
