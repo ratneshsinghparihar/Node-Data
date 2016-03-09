a8b44f49ff01f511016e23a364a50913c301afc5
ritesh <ritesh.agarwal@talentica.com>
2016-03-09 13:20:34 +0530

    ut

--- dynamic/authcontroller.spec.ts
+++ dynamic/authcontroller.spec.ts
@@ -1,3 +1,19 @@
-﻿describe('AuthControllerFunc',  () => {
+﻿/// <reference path="../unit-test/repository/user-repository-mock.ts" />
+
+import {AuthController} from './authcontroller';
+import {UserRepositoryMock} from '../unit-test/repository/user-repository-mock';
+
+describe('AuthControllerFunc', () => {
     var ut;
+    //it('Calls function createAuthStrategy', () => {
+    //    var authController = {
+    //        addRoutes: function () {
+    //        }
+    //    };
+
+    //    spyOn(authController, "addRoutes");
+    //    authController = new AuthController("/", <any>UserRepositoryMock);
+    //   // authController.createAuthStrategy();
+    //    expect(authController.addRoutes).toHaveBeenCalled();
+    //})
 });
