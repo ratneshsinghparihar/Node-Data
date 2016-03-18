622aa3e679662d563c11476300bff9e7efc95e68
Hari <hari.adhikari@talentica.com>
2016-03-18 12:59:44 +0530

    add tslint in the current project

--- mongoose/datatype.ts
+++ mongoose/datatype.ts
@@ -3,19 +3,7 @@
 export import ObjectId = Mongoose.Types.ObjectId;
 export import Mixed = Mongoose.SchemaType;
 
-var aa = {
-    String: String,
-    ObjectId: Object,
-    OId: Object,
-    Mixed: Object,
-}
-export class Schema {
-    public static Types: {
-        String: String;
-        ObjectId: Object;
-        OId: Object;
-        Mixed: any;
-    };
-
-    aa() { Schema.Types.Mixed; }
+export var Types = {
+    ObjectId: ObjectId,
+    Mixed: Mixed
 }
