e718f8dd6cf810068e24e80565290642c71ef03b
ritesh <ritesh.agarwal@talentica.com>
2016-03-30 15:51:47 +0530

    NOD-102 # test cases for authcontroller and authservice

--- security/auth/authcontroller.spec.ts
+++ security/auth/authcontroller.spec.ts
@@ -1,19 +1,40 @@
-﻿/// <reference path="../../unit-test/repository/user-repository-mock.ts" />
-/// <reference path="../../di/di.ts" />
-/// <reference path="auth-service.ts" />
-/// <reference path="../../unit-test/services/MockService.ts" />
-
+﻿require('reflect-metadata/reflect');
 import {AuthController} from './authcontroller';
 import {UserRepositoryMock} from '../../unit-test/repository/user-repository-mock';
 import {Container} from '../../di/di';
 import {AuthService} from './auth-service';
 import {MockAuthService} from '../../unit-test/services/MockService';
+import * as securityUtils from './security-utils';
+import * as configUtils from '../../core/utils';
 
 describe('AuthControllerFunc', () => {
     beforeEach(() => {
-        spyOn(AuthController.prototype, 'addRoutes');
-        spyOn(AuthController.prototype, 'createAuthStrategy');
-        spyOn(AuthService.prototype, 'addRoutes');
+        spyOn(Container, 'resolve').and.callFake((val) => {
+            switch (val) {
+                case AuthService:
+                    return new MockAuthService();
+            }
+        });
+        spyOn(configUtils, 'config').and.returnValue(
+            {
+                'Security': {
+                    'isAutheticationEnabled': 'disabled',
+                    'authenticationType': 'passwordBased'
+                },
+                'facebookAuth': {
+                    'clientID': '11',
+                    'clientSecret': 'aa',
+                    'callbackURL': 'http://localhost:23548/auth/facebook/callback'
+                },
+                'Config': {
+                    'DbConnection': 'mongodb://localhost:27017/userDatabase',
+                    'basePath': "data",
+                    'apiversion': "v1",
+                    'ElasticSearchConnection': 'http://localhost:9200',
+                    'ApplyElasticSearch': false
+                }
+            }
+        );
     });
 
     it('authController constructor', () => {
