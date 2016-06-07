ae68305772e8cebce76d7e8e75c0ca14bec530e7
pratik vakil <vakilpratik@gmail.com>
2016-06-07 18:02:29 +0530

    modified ci-version-devbuild

--- scripts/ci-version-devbuild.js
+++ scripts/ci-version-devbuild.js
@@ -1,19 +1,18 @@
-﻿var fs = require("fs");
-
-export function setNewDevBuildVersion(pkgJsonPath) {
+"use strict";
+const fs = require("fs");
+function setNewDevBuildVersion(pkgJsonPath) {
     let pkgJsonParsed = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
     let oldVersion = pkgJsonParsed.version;
     // Update the version
     let newDevVersion = getNextDevVersion(pkgJsonParsed.version);
     pkgJsonParsed.version = newDevVersion;
-
-    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJsonParsed, undefined, 2), {encoding: 'utf8'});
+    fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJsonParsed, undefined, 2), { encoding: 'utf8' });
     let pkgJsonParsedNew = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
     if (pkgJsonParsed.version === oldVersion) {
         throw 'Version update failed';
     }
 }
-
+exports.setNewDevBuildVersion = setNewDevBuildVersion;
 function getNextDevVersion(versionString) {
     // Get date and time string in the format YYMMDDHHMMSS
     let timeStamp = new Date().toISOString().replace(/[TZ:\-\.]/g, "").slice(2, 14);
