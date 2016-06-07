02b56ba0ce5954d24bd6d395b302521994da9801
pratik vakil <vakilpratik@gmail.com>
2016-06-07 17:56:36 +0530

    Removed ts for CI commit. Changed the extension to JS

--- scripts/ci-version-devbuild.js
+++ scripts/ci-version-devbuild.js
@@ -1,24 +1,20 @@
-﻿import fs = require("fs");
-
-interface PackageJson {
-    version: string;
-}
+﻿var fs = require("fs");
 
 export function setNewDevBuildVersion(pkgJsonPath) {
-    let pkgJsonParsed: PackageJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
+    let pkgJsonParsed = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
     let oldVersion = pkgJsonParsed.version;
     // Update the version
     let newDevVersion = getNextDevVersion(pkgJsonParsed.version);
     pkgJsonParsed.version = newDevVersion;
 
     fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJsonParsed, undefined, 2), {encoding: 'utf8'});
-    let pkgJsonParsedNew: PackageJson = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
+    let pkgJsonParsedNew = JSON.parse(fs.readFileSync(pkgJsonPath, 'utf8'));
     if (pkgJsonParsed.version === oldVersion) {
         throw 'Version update failed';
     }
 }
 
-function getNextDevVersion(versionString: string) {
+function getNextDevVersion(versionString) {
     // Get date and time string in the format YYMMDDHHMMSS
     let timeStamp = new Date().toISOString().replace(/[TZ:\-\.]/g, "").slice(2, 14);
     if (versionString.indexOf('-') != -1) {
