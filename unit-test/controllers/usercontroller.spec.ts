221111fc0f2fecefc2dfcc8be3ba785d951a3cf3
Asish-JavaRevisited <asish.nitr@gmail.com>
2017-01-02 20:47:48 +0530

    Fixing usercontroller.spec.ts

--- unit-test/controllers/usercontroller.spec.ts
+++ unit-test/controllers/usercontroller.spec.ts
@@ -3,7 +3,7 @@ var base_url = "http://localhost:23548/data/users";
 
 describe("GET Server", function(){
     describe("GET /users", function() {
-         it("returns status code 200", function() {
+         it("returns status code 200", function(done) {
              request.get(base_url, function(error, response, body) {
                  expect(response.statusCode).toBe(402);
                  done();
