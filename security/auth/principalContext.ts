899f629228924b797ac5e93423a61386ef40ccda
`mayank <maagrawal@gep.com>
2017-01-11 13:28:13 +0530

    Reverted continuation-local-storage changes. Instead used Domain.ts for principalContext

--- security/auth/principalContext.ts
+++ security/auth/principalContext.ts
@@ -1,77 +1,81 @@
-﻿﻿//var domain = require('./domain');
+﻿﻿var domain = require('./domain');
 var cls = require('continuation-local-storage');
 
 export class PrincipalContext {
-        private static session:any;
-    
+    private static session: any;
+
     static getSession() {
-       if (PrincipalContext.session != null && PrincipalContext.session != 'undefined') {
-           //cls = require('continuation-local-storage').getNamespace;
-           PrincipalContext.session = cls.getNamespace('session');
-       }else{
-           console.log('creating session from principal context');
-           //cls = require('continuation-local-storage').createNamespace;
-           PrincipalContext.session= cls.createNamespace('session');
-       }
-       return PrincipalContext.session;
+        if (PrincipalContext.session != null && PrincipalContext.session != 'undefined') {
+            //cls = require('continuation-local-storage').getNamespace;
+            PrincipalContext.session = cls.getNamespace('session');
+        } else {
+            console.log('creating session from principal context');
+            //cls = require('continuation-local-storage').createNamespace;
+            PrincipalContext.session = cls.createNamespace('session');
+        }
+        return PrincipalContext.session;
     }
 
     static get User(): any {
-        //return domain.get('context:user');
-        return PrincipalContext.getSession().get('user');
+        return domain.get('context:user');
+        //return PrincipalContext.getSession().get('user');
     }
 
     static set User(user: any) {
-        //domain.set('context:user', user);
-        PrincipalContext.setKeys('user');  
-        PrincipalContext.getSession().set('user', user);
+        domain.set('context:user', user);
+        PrincipalContext.setKeys('user');
+        //PrincipalContext.getSession().set('user', user);
     }
 
     static save(key: string, value: any): any {
         if (!key)
             throw 'invalid key';
-        //domain.set('context:' + key.trim(), value);
-        PrincipalContext.setKeys(key);        
-        PrincipalContext.getSession().set(key, value);
+        domain.set('context:' + key.trim(), value);
+        PrincipalContext.setKeys(key);
+        //PrincipalContext.getSession().set(key, value);
     }
 
     static get(key: string) {
-        //return domain.get('context:' + key.trim());
-        return PrincipalContext.getSession().get(key);
+        return domain.get('context:' + key.trim());
+        //return PrincipalContext.getSession().get(key);
     }
 
-    private static setKeys(key:String){
-        var keys :Array<any> = PrincipalContext.getSession().get('_keys');
-        if(keys==null){
-            PrincipalContext.getSession().set('_keys',[key]);
+    private static setKeys(key: String) {
+        var keys: Array<any> = domain.get('context:_keys');
+        //var keys :Array<any> = PrincipalContext.getSession().get('_keys');
+        if (keys == null) {
+            domain.set('context:_keys', [key]);
+            //PrincipalContext.getSession().set('_keys',[key]);
         }
-        else{
-            if(keys.indexOf(key)<0){
+        else {
+            if (keys.indexOf(key) < 0) {
                 keys.push(key);
-                PrincipalContext.getSession().set('_keys',keys);
+                domain.set('context:_keys', keys);
+                //PrincipalContext.getSession().set('_keys',keys);
             }
         }
         //console.log("context: "+ JSON.stringify(PrincipalContext.getSession()));
     }
 
-    static getAllKeys(){
-        return PrincipalContext.getSession().get('_keys');
+    static getAllKeys() {
+        return domain.get('context:_keys');
+        //return PrincipalContext.getSession().get('_keys');
     }
 
-    static getAllKeyValues():any{
-        var keys:Array<any> = PrincipalContext.getAllKeys();
+    static getAllKeyValues(): any {
+        var keys: Array<any> = PrincipalContext.getAllKeys();
         var ret = {};
-        keys.forEach(x=>{
-                var val = PrincipalContext.get(x);
-                        if(x && val){
-                            try{
-                                val = JSON.parse(val);
-                                }
-                            catch(e){
-                                    }
-                            ret[x]=val;
-                            }
-                        });
+        keys.forEach(x => {
+            var val = PrincipalContext.get(x);
+            if (x && val) {
+                try {
+                    val = JSON.parse(val);
+                }
+                catch (e) {
+                }
+                ret[x] = val;
+            }
+        });
         return ret;
     }
 }
