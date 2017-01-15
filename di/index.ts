b9209594052ef6e61e04e2ef35d0e6aab0c2a578
`mayank <maagrawal@gep.com>
2017-01-15 17:13:49 +0530

    Changes done for repository injection

--- di/index.ts
+++ di/index.ts
@@ -9,10 +9,20 @@ let repoSource = function (target: ClassType<any>) {
     }
     Object.keys(repositoryMap())
         .forEach(x => {
-            // TODO: Decide repository map will hold the prototype or the function
-            if (repositoryMap()[x].fn === target || <Function>repositoryMap()[x].fn === target.prototype|| (target.prototype && x === target.prototype.path)) {
+            //// TODO: Decide repository map will hold the prototype or the function
+            var path;
+            if (target.prototype) {
+                path = target.prototype.path;
+            }
+            else if (target['default']) {
+                path = target['default'].prototype.path;
+            }
+            if (path === x) {
                 repo = repositoryMap()[x].repo;
             }
+            //if (repositoryMap()[x].fn === target || <Function>repositoryMap()[x].fn === target.prototype|| (target.prototype && x === target.prototype.path)) {
+            //    repo = repositoryMap()[x].repo;
+            //}
         });
     return repo;
 }
