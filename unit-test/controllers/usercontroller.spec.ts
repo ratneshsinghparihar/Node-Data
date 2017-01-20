6f1c0b6ceef21bed79d7fc36e3e2b671f7df4236
Asish Kumar Sahoo <asish.nitr@gmail.com>
2017-01-20 14:47:01 +0530

    Fixing Unwanted contents
    Fixing Unwanted contents

--- unit-test/controllers/usercontroller.spec.ts
+++ unit-test/controllers/usercontroller.spec.ts
@@ -1,18 +1,4 @@
-// var request = require("request");
-// var base_url = "http://localhost:23548/data/users";
 
-<<<<<<< HEAD
-// describe("GET Server", function(){
-//     describe("GET /users", function() {
-//          it("returns status code 200", function() {
-//              request.get(base_url, function(error, response, body) {
-//                  expect(response.statusCode).toBe(402);
-//                  done();
-//              });
-//         });
-//     });
-// });
-=======
 describe("GET Server", function(){
     describe("GET /users", function() {
          it("returns status code 200", function(done) {
@@ -23,4 +9,3 @@ describe("GET Server", function(){
         });
     });
 });
->>>>>>> 91b7d446bc6afca4f600cd3b80e6a69f3f523e23
