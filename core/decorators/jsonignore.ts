10e1434e1ef584c16551e4708c5b7172aa1c69bd
anujarya <anujaryatitanic300@gmail.com>
2017-04-26 18:50:45 +0530

    Added help description for the jsonignore annotation.

--- core/decorators/jsonignore.ts
+++ core/decorators/jsonignore.ts
@@ -3,6 +3,13 @@ import {Decorators} from '../constants/decorators';
 import {DecoratorType} from '../enums/decorator-type';
 import {JsonIgnore} from '../enums/jsonignore-enum';
 
+
+/**
+ * Ignores the property for an update in db from frontend or any external api source.
+ * Params: An optional parameter, if no parameter supplied means the property will not be available in frontend at all.
+ * if parameter Jsonignore.READ means the property will be available to frontend side but readonly mode. (can't altered by any external api in db)
+ * @param params
+ */
 export function jsonignore(params?: JsonIgnore): any {
     params = params || <any>{};
     return function (target: Object, propertyKey: string) {
