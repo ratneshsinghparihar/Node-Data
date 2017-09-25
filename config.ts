9649a1acef7e15e944782e4d8d1a143023e904cb
anujarya <anujaryatitanic300@gmail.com>
2017-09-26 00:23:54 +0530

    domainsEnabled setting enabled to support mongoose latest upgrade.

--- config.ts
+++ config.ts
@@ -1,5 +1,9 @@
+
+let DbConnectionOptions = { server: { poolSize: 20, domainsEnabled: true } };
+
 export class Config {
     public static DbConnection: string = "mongodb://localhost:27017/test_nodedata";
+    public static DbConnectionOptions = DbConnectionOptions;
     public static basePath: string = "data";
     public static apiversion: string = "v1";
 
