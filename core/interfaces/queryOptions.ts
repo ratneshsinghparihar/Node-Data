b276ae8c3f5c75feb6754a892b8493150a8d102b
bhavesh <bhavesh.patel@zebpay.com>
2017-12-21 12:31:52 +0530

    ratnesh changes for support find queries in socket

--- core/interfaces/queryOptions.ts
+++ core/interfaces/queryOptions.ts
@@ -1,4 +1,5 @@
- export interface QueryOptions {
+import * as Enumerable from 'linq';
+export interface QueryOptions {
     rows?: number;
     start?: number;
     from?: Date;
@@ -16,6 +17,39 @@
     gte?: any;
     lte_value?: any;
     gte_value?: any;
+}
+
+export var getQueryOptionsFromQuery = (query): { queryObj: any, options: any }=>{
+    var queryObj = query;
+    var options = {};
+    Enumerable.from(queryObj).forEach((x: any) => {
+        if (filterProps.indexOf(x.key) >= 0) {
+            options[x.key] = x.value;
+            delete queryObj[x.key];
+        }
+        else {
+            var val = queryObj[x.key];
+            var i = val.indexOf('%LIKE%');
+            if (i == 0) {
+                // contains
+                val = val.replace('%LIKE%', '');
+                queryObj[x.key] = {
+                    $regex: '.*' + val + '.*'
+                }
+            }
+            else {
+                i = val.indexOf('%START%');
+                if (i == 0) {
+                    // starts with
+                    val = val.replace('%START%', '');
+                    queryObj[x.key] = {
+                        $regex: '^' + val + '.*'
+                    }
+                }
+            }
+        }
+    });
+    return { queryObj: queryObj, options: options}
 }
 
 export var filterProps: Array<string> = ['rows', 'start', 'from', 'until', 'order', 'fields', 'skip', 'limit', 'sort','lt','gt','lte','gte','lt_value','gt_value','lte_value','gte_value'];
