3fc9905d0fab9e13d473be4f4cdc49e3dab13ca5
mayank <mayank.agrawal@talentica.com>
2019-02-05 16:44:55 +0530

    Find and updated base class properties of sequelize objects

--- core/index.ts
+++ core/index.ts
@@ -12,6 +12,7 @@ var domain = require('../security/auth/domain');
 
 //import linq = require('../typings/linq/linq');
 import * as Enumerable from 'linq';
+import { Decorators } from './constants';
 
 // Ignore .d.ts and all other non .ts files
 function ignoreFile(file: string) {
@@ -82,7 +83,8 @@ module.exports = function (config: any, securityConfig: any, appRoot?: string,
     //Utils.entityService(entityServiceInst);
     //Utils.sqlEntityService(sqlServerInst);
     new Dynamic(config, securityConfig,server);
-    MetaUtils.refreshDerivedObjectsMetadata();
+    MetaUtils.refreshDerivedObjectsMetadata(Decorators.DOCUMENT);
+    MetaUtils.refreshDerivedObjectsMetadata(Decorators.ENTITY);
 }
 
 let components: Array<any> = [];
