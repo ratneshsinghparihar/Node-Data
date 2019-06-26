31236e0d11020c110113c7e774bbb7d0294c4322
Anuj Rana <anujaryatitanic300@gmail.com>
2019-06-26 16:10:40 +0530

    added capability to set pluralization and default lowercase off for a collection name.

--- mongoose/decorators/interfaces/document-params.ts
+++ mongoose/decorators/interfaces/document-params.ts
@@ -3,4 +3,10 @@ import {IDecoratorParams} from '../../../core/decorators/interfaces/decorator-pa
 
 export interface IDocumentParams extends IDecoratorParams {
     strict: Strict;
+    /**
+     * Set false if you want to use same name as the collection name
+     * (means letter case conversion and pluralization 
+     * )
+     */
+    pluralization: boolean;
 }
